89aed0da385da4b96d654a6badab787db612b8e0
leonardobocci <leonardobocci99@hotmail.com>
2025-04-21 20:30:42 +0200

    remove items mgmt from frontend

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,15 +1,13 @@
 import { Box, Flex, Icon, Text } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link as RouterLink } from "@tanstack/react-router"
-import { FiBriefcase, FiHome, FiSettings, FiUsers, FiUpload } from "react-icons/fi"
+import { FiSettings, FiUsers, FiUpload } from "react-icons/fi"
 import type { IconType } from "react-icons/lib"
 
 import type { UserPublic } from "@/client"
 
 const items = [
-  { icon: FiHome, title: "Dashboard", path: "/" },
-  { icon: FiBriefcase, title: "Items", path: "/items" },
-  { icon: FiUpload, title: "Upload", path: "/upload" },
+  { icon: FiUpload, title: "Upload Files", path: "/" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
 
